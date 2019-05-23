151f45eceff0376321a267c3a0e346f261b7bade
yellowgg <yellowgggg@gmail.com>
2019-05-23 20:02:18 +0800

    第四章 添加了快排的单边循环法

--- src/main/java/cn/yellowgg/fourthchapter/QuickSort.java
+++ src/main/java/cn/yellowgg/fourthchapter/QuickSort.java
@@ -15,7 +15,7 @@ public class QuickSort {
             return;
         }
         //得到基准元素位置
-        int pivotIndex = bilateralPartition(arr, startIndex, endIndex);
+        int pivotIndex = unilateralPartition(arr, startIndex, endIndex);
         //根据基准元素，分成两部分进行递归排序
         quickSort(arr, startIndex, pivotIndex - 1);
         quickSort(arr, pivotIndex + 1, endIndex);
@@ -59,6 +59,39 @@ public class QuickSort {
 
         //返回基准位置(left和right都是一个值)
         return left;
+    }
+
+    /**
+     * 分治 (单边循环法)
+     *
+     * @param arr        待交换的数组
+     * @param startIndex 起始下标
+     * @param endIndex   结束下标
+     * @return
+     */
+    private static int unilateralPartition(int[] arr, int startIndex,
+                                           int endIndex) {
+        //取第一个元素（也可以选择随机元素）作为基准元素
+        int pivot = arr[startIndex];
+        int mark = startIndex;
+
+        for (int i = startIndex + 1; i <= endIndex; i++) {
+            if (arr[i] < pivot) {
+                //mark指针右移代表区域边界+1
+                mark++;
+                //交换两者的值
+                int temp = arr[mark];
+                arr[mark] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        //交换mark元素跟基准元素
+        arr[startIndex] = arr[mark];
+        arr[mark] = pivot;
+
+        //返回基准元素的新位置
+        return mark;
     }
 
     public static void main(String[] args) {
