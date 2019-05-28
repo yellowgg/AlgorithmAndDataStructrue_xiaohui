baee2cb57b954239f1adc7f99c484d49b93a34df
yellowgg <yellowgggg@gmail.com>
2019-05-28 16:16:06 +0800

    第五章 添加了求环长的方法

--- src/main/java/cn/yellowgg/fifthchapter/ListRing.java
+++ src/main/java/cn/yellowgg/fifthchapter/ListRing.java
@@ -1,4 +1,4 @@
-package cn.yellowgg.fifthChapter;
+package cn.yellowgg.fifthchapter;
 
 /**
  * @Author:黄广
@@ -43,17 +43,56 @@ public class ListRing {
         return false;
     }
 
+    /**
+     * 获取有环链表的环长度
+     *
+     * @param head 链表头节点
+     */
+    public static int getCycleLength(Node head) {
+        Node p1 = head;
+        Node p2 = head;
+        while (p2 != null && p2.next != null) {
+            //p1移动一个节点
+            p1 = p1.next;
+            //p2移动两个节点
+            p2 = p2.next.next;
+            //当追上的时候，开始一个新的循环
+            if (p1 == p2) {
+                break;
+            }
+        }
+        int length = 0;
+        while (p2 != null && p2.next != null) {
+            p1 = p1.next;
+            p2 = p2.next.next;
+            length++;
+            if (p1 == p2) {
+                return length;
+            }
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
         Node node1 = new Node(5);
         Node node2 = new Node(3);
         Node node3 = new Node(7);
         Node node4 = new Node(2);
         Node node5 = new Node(6);
+        Node node6 = new Node(8);
+        Node node7 = new Node(1);
         node1.next = node2;
         node2.next = node3;
         node3.next = node4;
         node4.next = node5;
-        node5.next = node2;
-        System.out.println(isCycle(node1));
+        node5.next = node6;
+        node6.next = node7;
+        node7.next = node4;
+        if (isCycle(node1)) {
+            System.out.println("该链表是有环链表");
+            System.out.println("环长为" + getCycleLength(node1));
+        } else {
+            System.out.println("该链表不是有环链表");
+        }
     }
 }
