ae4c9c88f6e4dcf6dc51fec651817eea34a543eb
yellowgg <yellowgggg@gmail.com>
2019-05-31 14:22:15 +0800

    第五章 无序数组排序后的最大相邻差

---
+++ src/main/java/cn/yellowgg/fifthchapter/MaxAdjacentDiff.java
@@ -0,0 +1,84 @@
+package cn.yellowgg.fifthchapter;
+
+/**
+ * @Author:黄广
+ * @Description: 无序数组排序后的最大相邻差
+ * @Date: Created in 19-5-31 下午1:55
+ */
+public class MaxAdjacentDiff {
+    /**
+     * 具体有三种解法，详情参考第五章笔记
+     */
+
+    /**
+     * 利用桶排序的思想获取最大相邻差，时间复杂度稳定在O(n)
+     *
+     * @param array
+     * @return
+     */
+    public static int getMaxSortedDistance(int[] array) {
+        //1.得到数列的最大最小值
+        int max = array[0];
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        int d = max - min;
+        //如果max和min相等，则说明数组所有元素都相等，返回0
+        if (d == 0) {
+            return 0;
+        }
+
+        //2.初始化桶
+        int bucketNum = array.length;
+        Bucket[] buckets = new Bucket[bucketNum];
+        for (int i = 0; i < bucketNum; i++) {
+            buckets[i] = new Bucket();
+        }
+
+        //3.遍历原始数组，确定每个桶的最大最小值
+        for (int i = 0; i < array.length; i++) {
+            //确定数组元素所归属的桶下标
+            int index = ((array[i] - min) * (bucketNum - 1) / d);
+            if (buckets[index].min == null || buckets[index].min > array[i]) {
+                buckets[index].min = array[i];
+            }
+            if (buckets[index].max == null || buckets[index].max < array[i]) {
+                buckets[index].max = array[i];
+            }
+        }
+
+        //4.遍历桶，找到最大差值
+        int leftMax = buckets[0].max;
+        int maxDistance = 0;
+        for (int i = 1; i < buckets.length; i++) {
+            if (buckets[i].min == null) {
+                continue;
+            }
+            if (buckets[i].min - leftMax > maxDistance) {
+                maxDistance = buckets[i].min - leftMax;
+            }
+            leftMax = buckets[i].max;
+        }
+
+        return maxDistance;
+    }
+
+    /**
+     * 桶
+     */
+    private static class Bucket {
+        Integer min;
+        Integer max;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{2, 6, 4, 5, 3, 10, 9};
+        System.out.println(getMaxSortedDistance(array));
+    }
+}
